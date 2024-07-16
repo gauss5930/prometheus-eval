1909c00592aa79dea0a92a90a17cba196a29e9f0
gauss5930 <kopilot100@gmail.com>
2024-07-17 01:34:24 +0900

    FLASK_prompt Commit

--- BiGGen-Bench/run_response_eval.py
+++ BiGGen-Bench/run_response_eval.py
@@ -13,6 +13,58 @@ from prometheus_eval.prompts import ABSOLUTE_PROMPT, SCORE_RUBRIC_TEMPLATE
 from prometheus_eval.vllm import VLLM
 from transformers import AutoTokenizer
 
+flask_eval_prompt = {
+    "logical_correctness": """Score 1: The model’s final answer is completely incorrect and lacks sound reasoning.
+Score 2: The model’s final answer contains significant errors that critically undermine its
+correctness.
+Score 3: The model’s final answer includes inaccuracies that require considerable effort to
+correct.
+Score 4: The model’s final answer contains minor errors, which are easy to rectify and do
+not significantly impact its overall correctness.
+Score 5: The model’s final answer is completely accurate and sound.""",
+    "factuality": """Score 1: The model did not extract pertinent background knowledge and provided inaccurate
+or misleading information. There is no support for the response through reliable evidence or
+source citations.
+Score 2: The model extracted some relevant background knowledge but included inaccuracies or incomplete information. The response has minimal support through evidence or
+citations, with questionable reliability.
+Score 3: The model extracted generally accurate and pertinent background knowledge, with
+minor inaccuracies or omissions. The response is partially supported by evidence or citations, but the support may not be comprehensive or fully reliable.
+Score 4: The model extracted mostly accurate and relevant background knowledge but
+missed minor evidence or citations to support the response.
+Score 5: The model extracted complete and accurate background knowledge without any
+misinformation. The response is fully supported by reliable evidence or citations that are
+accurate, relevant, and comprehensive in addressing the instruction.""",
+    "comprehension": """Score 1: The response is completely unrelated to the instruction, or the model entirely misunderstands the instruction.
+Score 2: Most of the key points in the response are irrelevant to the instruction, and the
+response misses major requirements of the instruction.
+Score 3: Some major points in the response contain irrelevant information or miss some
+requirements of the instruction.
+Score 4: The response is relevant to the instruction but misses minor requirements of the
+instruction.
+Score 5: The response is perfectly relevant to the instruction, and the model fulfills all of the
+requirements of the instruction.""",
+    "completeness": """Score 1: The response doesn’t include any specifics or examples to support the statements
+made.
+Score 2: The response does not provide sufficient details or supportive examples, requiring
+a major effort to make the response more complete.
+Score 3: It is a decent response, but the breadth and depth of the response are rather limited.
+The details and examples used to substantiate the response may be insufficient.
+Score 4: The response provides detailed explanations, but there is room for enhancement.
+The response could be further improved by including more details and supportive examples.
+Score 5: The response fully provides comprehensive explanations. It delves deep into the
+topic, providing as much detail as possible, and it offers several examples to back up its
+points.""",
+    "insightfulness": """Score 1: The response is overly simplistic, lacking any originality or novelty.
+Score 2: The ideas or perspectives within the response are commonplace, demonstrating a
+lack of originality or novelty.
+Score 3: Some may perceive the response as original and novel, but others may find it
+ordinary or uninspiring.
+Score 4: The response includes some innovative perspectives or ideas that require thoughtful
+consideration, yet they aren’t particularly surprising.
+Score 5: The response is infused with surprisingly creative perspectives or ideas that are
+challenging to conceive, showcasing significant originality and novelty."""
+}
+
 ABSOLLUTE_REFINE_PROMPT = """###Task Description:
 An instruction (might include an Input inside it), a response to evaluate, a reference answer that gets a score of 5, and a score rubric representing a evaluation criteria are given.
 1. Write a detailed feedback that assess the quality of the response strictly based on the given score rubric, not evaluating in general.
@@ -61,6 +113,7 @@ def main(args):
     input_file_path = args.input_file_path
     output_file_path = args.output_file_path
     eval_model_name = args.model_name
+    flask_eval_type = args.flask_eval_type
 
     is_prometheus = False
 
@@ -92,14 +145,14 @@ def main(args):
             instructions_2.append(record["input"])
             responses_2.append(record["response"])
             reference_answers_2.append(record["reference_answer"])
-            score_rubric = SCORE_RUBRIC_TEMPLATE.format(**record["score_rubric"])
+            score_rubric = flask_eval_prompt[flask_eval_type]
             rubric_2.append(score_rubric)
         else:
             records.append(record)
             instructions.append(record["input"])
             responses.append(record["response"])
             reference_answers.append(record["reference_answer"])
-            score_rubric = SCORE_RUBRIC_TEMPLATE.format(**record["score_rubric"])
+            score_rubric = flask_eval_prompt[flask_eval_type]
             rubric.append(score_rubric)
 
     assert (
@@ -170,6 +223,11 @@ if __name__ == "__main__":
         type=str,
         default="prometheus-eval/prometheus-7b-v2.0",
         help="Model name to use as evaluation model",
+    )
+    parser.add_argument(
+        "--flask_eval_type",
+        type=str,
+        default="logical_correctness"
     )
     parser.add_argument(
         "--input_file_path",
